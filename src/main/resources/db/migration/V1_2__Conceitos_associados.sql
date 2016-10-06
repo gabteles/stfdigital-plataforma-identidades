6b09c0385e3ccd8904437c4589ac4ea030555c11
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-10-06 16:45:37 -0300

    Criando o schema corporativo.

--- src/main/resources/db/migration/V1_2__Conceitos_associados.sql
+++ src/main/resources/db/migration/V1_2__Conceitos_associados.sql
@@ -1,3 +1,5 @@
+create schema if not exists corporativo;
+
 create sequence corporativo.seq_pessoa start with 1 nomaxvalue minvalue 1 nocycle nocache;
 create table corporativo.pessoa(
     seq_pessoa number not null,
